71f52996526618d8ff625a978dffb761757a3c0c
Gilad Madar <161640833+GiladMadar@users.noreply.github.com>
2024-07-17 20:31:58 +0300

    Delete Old Exam 2/Old directory

--- Old Exam 2/Old/PaymentType.py
+++
@@ -1,7 +0,0 @@
-
-class PaymentType:
-    def __init__(self, CREDIT_CARD, CASH, CHECK, OTHER):
-        self.CREDIT_CARD = CREDIT_CARD
-        self.CASH = CASH
-        self.CHECK = CHECK
-        self.OTHER = OTHER
